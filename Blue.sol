022b7a645e557afa7192810a29c0e060d537781b
Zhiyu <61263479+CyptoSuperNFT@users.noreply.github.com>
2023-03-28 23:14:03 +0800

    Update Blue.sol

--- Blue.sol
+++ Blue.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: CC-BY-SA-4.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.0/contracts/token/ERC20/ERC20.sol";
